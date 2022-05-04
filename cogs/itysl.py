1a79330aefc151b0dec61ccc590a454bfcb28b6f
Mark <markarneman@gmail.com>
2022-05-03 21:29:05 -0400

    I THINK YOU SHOULD LEAVE

--- cogs/itysl.py
+++ cogs/itysl.py
@@ -25,6 +25,13 @@ class ITYSL(commands.Cog):
     async def get_random_sketch(self):
         return random.choice(self.sketches)
 
+    async def get_quotes(self, sketch):
+        quotables = sketch["Transcript"].split(".")
+        return [
+            random.choice(quotables).strip()[:1024],
+            random.choice(quotables).strip()[:1024],
+        ]
+
     # Create slash command for /itysl
     @commands.slash_command(
         name="itysl", description="I THINK YOU SHOULD LEAVE: QUOTE AND MEMES"
@@ -35,20 +42,30 @@ class ITYSL(commands.Cog):
         embed = discord.Embed(title=sketch["Name"])
 
         # Create quotes
-        quotables = sketch["Transcript"].split(".")
-        embed.add_field(name="Quote 1", value=random.choice(quotables), inline=True)
-        embed.add_field(name="Quote 2", value=random.choice(quotables), inline=True)
-        embed.add_field(name="Quote 3", value=random.choice(quotables), inline=True)
+        quotes = await self.get_quotes(sketch)
+
+        embed.add_field(name="Quote 1:", value=quotes[0], inline=False)
+        embed.add_field(name="Quote 2:", value=quotes[1], inline=False)
 
         # Embed image
         if sketch["Image"] is not None:
             embed.set_image(url=sketch["Image"])
 
+        embed.set_author(
+            name="Tim Robinson", icon_url="https://i.imgur.com/QcVgX2N.png"
+        )
+        embed.set_thumbnail(
+            url="https://upload.wikimedia.org/wikipedia/en/d/d4/I_Think_You_Should_Leave.png"
+        )
+
+        # Add timestamp
+        embed.add_field(name="Timestamp:", value=sketch["Link"], inline=False)
+
         embed.set_footer(
             text="Season " + sketch["Season"] + " Episode " + sketch["Episode"]
         )
 
-        await ctx.respond("I THINK YOU SHOULD LEAVE", embed=embed)
+        await ctx.respond(" ", embed=embed)
 
 
 # Py-Cord calls setup on load
