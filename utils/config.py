9a90ca686d3f9f3952ed1a1f94d49b82402e5628
Mark Arneman <markarneman@gmail.com>
2023-06-26 09:41:15 -0400

    Config cleanup for Render

--- utils/config.py
+++ utils/config.py
@@ -1,14 +1,6 @@
-
-import logging
 import yaml
-import os
 
 def load():    
-    # Error Handling, what if config.yaml doesn't exist?
-    if(os.path.isfile("config.yaml") == False):
-        print("[ERROR]: 'config.yaml' does not exist!")
-        exit()
-
     # Load yaml config
     with open("config.yaml", "r") as config:
         config = yaml.safe_load(config)
